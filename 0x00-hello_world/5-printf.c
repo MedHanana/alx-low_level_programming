926bc619172c4aa320a229aba0e3f0a17c36ff89
mohamed hanana <mohammedhanana1@gmail.com>
2023-07-17 16:58:00 -0700

    5 task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'C Program'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
