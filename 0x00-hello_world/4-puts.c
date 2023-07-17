d5a5fe5a893906ba45580c3fcbfef002817d43bb
mohamed hanana <mohammedhanana1@gmail.com>
2023-07-17 16:53:49 -0700

    4 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * return : always 0 (success)
+ * Description: 'C Program'
+ *
+ * Return: Always 0 (Success)
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-			return(0);
-			}
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
