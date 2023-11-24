15c55e8f79f93f76574eae32f3a5c17aba81827e
MedHanana <mohammedhanana1@gmail.com>
2023-11-23 17:28:13 -0800

    1st  task1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	if (!b)
 		return (0);
-	while (*b)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[a] != '0' && b[a] != '1')
 			return (0);
-		num = num * 2 +(*b++ - '0');
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
 	}
 	return (num);
 }
